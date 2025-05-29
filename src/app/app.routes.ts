83c4e56223f511c702fcfab250c4a2708330948e
Vehuel011092 <86485137+Vehuel011092@users.noreply.github.com>
2025-05-28 17:46:46 -0700

    Se agrego animacion dropdown y se añadio un crud para usuarios

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,12 @@ import { LoginComponent } from './auth/login/login.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { authGuard } from './services/auth.guard'; // Importa el guardia de autenticación
 import { MainContentComponent } from './dashboard/main-content/main-content.component';
-import { UsersListComponent } from './dashboard/components/users/users-list/users-list.component';
+import { UserListComponent } from './dashboard/components/users/users-list/users-list.component';
 import { TicketsListComponent } from './dashboard/components/tickets/tickets-list/tickets-list.component';
 // Este archivo define las rutas de la aplicación Angular.
 
 export const dashboardRoutes: Routes = [
-  { path: 'users', component: UsersListComponent },
+  { path: 'users', component: UserListComponent },
   { path: 'tickets', component: TicketsListComponent },
   { path: '', component: MainContentComponent }, // Ruta por defecto
   { path: '**', component: MainContentComponent } // Ruta por defecto
