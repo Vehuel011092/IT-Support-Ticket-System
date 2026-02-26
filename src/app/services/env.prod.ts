aa2e9a1365033f369a3b4a64b1b08d05aac69b73
Xenovia01 <angel.antonio.rios.picaso@gmail.com>
2026-02-26 14:07:37 -0800

    new url

--- src/app/services/env.prod.ts
+++ src/app/services/env.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'back-end-itsis-production.up.railway.app' // Ajusta al nombre real de tu backend
+  apiUrl: 'https://back-end-itsis.up.railway.app' // Ajusta al nombre real de tu backend
 };
